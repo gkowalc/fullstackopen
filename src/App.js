5c5d2f0f47fde3dc4b061f048d8471066f0bbfa5
gkowal <gkowal@protonmail.com>
2021-08-27 13:29:39 +0200

    week1, exerice1.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './Header'
 const App = () => {
-  const course = {name: 'Half Stack application development', 
-parts: {
-  
-}}
+  const course = {coursename: 'Half Stack application development', 
+  part1: 'Fundamentals of React',
+  exercises1: 10,
+  part2: 'Using props to pass data',
+  exercises2: 7,
+  part3: 'State of a component',
+  exercises3: 14
+}
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -14,18 +18,15 @@ parts: {
 
   const propslist = {part1, part2, part3, exercises1, exercises2, exercises3}
 const exerciseslist = {exercises1, exercises2, exercises3}
- const Content = (props) => {
-   return(<div>
-    <p>
-    {propslist.part1} {propslist.exercises1}
-  </p>
-  <p>
-    {propslist.part2} {propslist.exercises2}
-  </p>
-  <p>
-    {propslist.part3} {propslist.exercises3}
-  </p></div>
-   )
+
+ const Part = (props) => {
+
+  return (
+    <div> {props.title}: {props.exercise}
+
+    </div>
+  )
+
  }
  const Exercises = (props) => {
    return (
@@ -37,8 +38,10 @@ const exerciseslist = {exercises1, exercises2, exercises3}
  }
   return (
     <div>
-      <Header course={course.name} />
-      <Content propslist={propslist}></Content>
+      <Header course={course.coursename} />
+      <Part title={course.part1} exercise={course.exercises1}/>
+      <Part title={course.part2} exercise={course.exercises2}/>
+      <Part title={course.part3} exercise={course.exercises3}/>
       <Exercises exerciseslist={exerciseslist}/>
       
     </div>
