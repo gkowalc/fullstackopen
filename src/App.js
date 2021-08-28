38f5c28983ea2d0bc7bf4a75c73aebaf5b347059
gkowal <gkowal@protonmail.com>
2021-08-28 19:53:30 +0200

    week1 1.8 exercise

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react'
+const Statistics = (props) => {
+  return (<div>
+   <p>total: {props.statisticpropdata.good + props.statisticpropdata.neutral + props.statisticpropdata.bad}</p>
+<p>Average: {(props.statisticpropdata.good - props.statisticpropdata.bad)/(props.statisticpropdata.good + props.statisticpropdata.neutral +props.statisticpropdata.bad )}</p>
+<p>Positive: {(props.statisticpropdata.good/(props.statisticpropdata.good + props.statisticpropdata.neutral +props.statisticpropdata.bad ))* 100}</p>
 
+  </div>)
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const calculateAverage = () => {
-      var score = (good - bad)/(good + neutral +bad )
-      return score  
 
 
-  }
+
+
+  const Statisticprops = {good:good, neutral: neutral, bad: bad};
+  
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -19,11 +26,7 @@ const App = () => {
       <p>Good: {good} </p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
-      <p>total: {good + neutral + bad}</p>
-
-      <p>Average: {(good - bad)/(good + neutral +bad )}</p>
-
-      <p>Positive: {(good/(good + neutral +bad ))* 100}</p>
+      <Statistics  statisticpropdata = {Statisticprops}/>
     
     </div>
   )
