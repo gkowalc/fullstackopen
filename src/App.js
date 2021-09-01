e9caa6c28b33741a886506ef3b183d87498e0de9
Greg <gkowal@protonmail.com>
2021-09-01 19:59:29 +0200

    ful  stack open week1 1.14*

--- src/App.js
+++ src/App.js
@@ -12,37 +12,45 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
   const  [points, setpoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
-
   const [currentindex, setcurrentindex] = useState(0)
-
   const [selected, setSelected] = useState(0)
+const [highestvalue, sethighestvalue] = useState()
   const randomSelect = () => {
-
-
     const randomElement = anecdotes[Math.floor(Math.random() * anecdotes.length)]
-
     setSelected(randomElement)
     setcurrentindex(anecdotes.indexOf(randomElement))
     render()
     console.log(randomElement);
   }
+
+
+  
 const addvote = () => {
 const copy = {...points}
 copy[currentindex] += 1
 setpoints(copy)
+//https://stackoverflow.com/questions/40561199/es6-how-to-define-functions-inside-a-const
+var maxkey = Object.keys(points).reduce((a, b) => points[a] > points[b] ? a : b);
 
+console.log(maxkey)
+sethighestvalue(maxkey)
+render()
 
 }
 
   return (
     <div>
 
-      
+      <h1>Anecdote of the day</h1>
       {selected}
       <p>
       <button onClick={randomSelect}> next anecdote </button></p> 
       <p>has {points[currentindex]} votes</p>
       <p><button onClick={addvote} >Vote</button></p>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[highestvalue]}
+      <p>has {points[highestvalue]} votes</p>
+
     </div>
   )
 }
