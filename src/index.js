8c719096571721c6194ef62159f186bf3683bc21
Greg <gkowal@protonmail.com>
2021-09-03 17:52:37 +0200

    week2, exercise 2.5

--- src/index.js
+++ src/index.js
@@ -1,41 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Courses from './Courses';
 
-
-const Courses = ({props}) => {
-const course_name = props.map((n)=> <><Header course={n}/> <Content course={n} /> <Totals course={n}/></>)
- return (
-   <div>{course_name}</div>
- )
-
-}
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-const Part = (props) => {
-  return ( <div>
-    {props.name}: {props.exercicesprops}
-    </div>
-  )
-}
-
-const Content = ({ course }) => {
-  const part_function =  course.parts.map((n, key_prop) =>  <Part key={key_prop} name={n.name} exercicesprops={n.exercises}></Part>);
-  return (
-    <>
-     {part_function} 
-    </>
-  )
-  
-}
-
-const Totals = ({course}) => {
-  const totalscore = course.parts.reduce((previousscore, nextvalue)=> previousscore+nextvalue.exercises, 0); 
-  return (
-  <div>Total Score: {totalscore}</div>)
-};
 
 
 
