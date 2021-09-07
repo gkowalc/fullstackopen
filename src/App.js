de99731d898b3461873cad6daedc97a5219e4f06
Greg <gkowal@protonmail.com>
2021-09-07 14:05:21 +0200

    week2 exercise2.6

--- src/App.js
+++ src/App.js
@@ -1,56 +1,44 @@
-import { render } from '@testing-library/react'
-import React, { useState, } from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-  const  [points, setpoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
-  const [currentindex, setcurrentindex] = useState(0)
-  const [selected, setSelected] = useState(0)
-const [highestvalue, sethighestvalue] = useState()
-  const randomSelect = () => {
-    const randomElement = anecdotes[Math.floor(Math.random() * anecdotes.length)]
-    setSelected(randomElement)
-    setcurrentindex(anecdotes.indexOf(randomElement))
-    render()
-    console.log(randomElement);
+  const [ persons, setPersons ] = useState([
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handlerNameChange = (event) => {
+setNewName(event.target.value)
+
+
   }
 
+const addNewName = (event) => {
+  event.preventDefault()
+  const nameObject = {
+    name: newName,
+    date: new Date().toISOString(),
+    id: persons.length + 1,
+  }
+  setPersons(persons.concat(nameObject))
+  console.log(persons)
 
-  
-const addvote = () => {
-const copy = {...points}
-copy[currentindex] += 1
-setpoints(copy)
-//https://stackoverflow.com/questions/40561199/es6-how-to-define-functions-inside-a-const
-var maxkey = Object.keys(points).reduce((a, b) => points[a] > points[b] ? a : b);
-
-console.log(maxkey)
-sethighestvalue(maxkey)
-render()
-
-}
-
+}  
   return (
     <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handlerNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addNewName}>add</button>
+        </div>
+      </form>
 
-      <h1>Anecdote of the day</h1>
-      {selected}
-      <p>
-      <button onClick={randomSelect}> next anecdote </button></p> 
-      <p>has {points[currentindex]} votes</p>
-      <p><button onClick={addvote} >Vote</button></p>
-      <h1>Anecdote with most votes</h1>
-      {anecdotes[highestvalue]}
-      <p>has {points[highestvalue]} votes</p>
-
+      <h2>
+    Numbers</h2>
+      
+      {persons.map(prop => <ul key={prop.id}> {prop.name}  </ul> )}
+      
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,65 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Courses from './Courses';
+import ReactDOM from 'react-dom'
+import App from './App.js'
 
 
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 
-
-
-
-
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-  
-
-  return (
-    <div>
-     <Courses props={courses}/>
-       </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
