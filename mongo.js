08573bd3c34243f2be0d0d1b2a2e09d3a7d4b959
Greg <gkowal@protonmail.com>
2021-09-29 15:35:34 +0200

    uibuild

---
+++ mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://kasdja17kkaa1111:${password}@cluster0.eqb3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const PersonSchema = new mongoose.Schema({
+  name: String,
+  date: Date,
+  number: Integer,
+})
+
+const Person = mongoose.model('Note', PersonSchema)
+
+const person = new Person({
+  name: "Firstname Lastname",
+  date: new Date(),
+  number: 945745473,
+})
+
+person.save().then(result => {
+  console.log('added!' + person.name + person.number + "To the phonebook")
+  mongoose.connection.close()
+})
