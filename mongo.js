1de25bb78e60bdd45166def35e6c70fe25aa15bf
Greg <gkowal@protonmail.com>
2021-09-29 16:07:19 +0200

    uibuild

--- mongo.js
+++ mongo.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
-<<<<<<< HEAD
-global.TextEncoder = require("util").TextEncoder; 
-=======
->>>>>>> phonebook_backend
+
 if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
@@ -14,27 +11,6 @@ const url =
   `mongodb+srv://kasdja17kkaa1111:${password}@cluster0.eqb3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(url)
-
-<<<<<<< HEAD
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
-=======
 const PersonSchema = new mongoose.Schema({
   name: String,
   date: Date,
@@ -50,9 +26,7 @@ const person = new Person({
 })
 
 person.save().then(result => {
-  console.log('added' + person.name + person.number + "To the phonebook")
+  console.log('added!!' + person.name + person.number + "To the phonebook")
   mongoose.connection.close()
 })
 
-
->>>>>>> phonebook_backend
