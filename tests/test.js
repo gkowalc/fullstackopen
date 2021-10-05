4add132ec34a0ae0c9e8ab676e935612813327c9
Greg <gkowal@protonmail.com>
2021-10-05 13:22:21 +0200

    4.3 modules done

---
+++ tests/test.js
@@ -0,0 +1,45 @@
+
+const palindrome = require('../utils/for_testing').palindrome
+
+test('palindrome of a', () => {
+  const result = palindrome('a')
+
+  expect(result).toBe('a')
+})
+
+test('palindrome of react', () => {
+  const result = palindrome('react')
+
+  expect(result).toBe('tcaer')
+})
+
+test('palindrome of releveler', () => {
+  const result = palindrome('releveler')
+
+  expect(result).toBe('releveler')
+})
+
+const average = require('../utils/for_testing').average
+
+describe('average', () => {
+  test('of one value is the value itself', () => {
+    expect(average([1])).toBe(1)
+  })
+
+  test('of many is calculated right', () => {
+    expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5)
+  })
+
+  test('of empty array is zero', () => {
+    expect(average([])).toBe(0)
+  })
+})
+
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
