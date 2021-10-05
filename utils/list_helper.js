442268829bc13b5ba1deb5c72925cd44048e65be
Greg <gkowal@protonmail.com>
2021-10-05 22:13:52 +0200

    4.5 module done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -18,16 +18,30 @@ const totalLikes =  (blogs) => {
     return sumOfLikes
     }
     
-       
-      
-
-      
-
 }  
-
-
+const favoriteBlog =  (blogs) => {
+    if (blogs.length === 1) {
+    for (const val of blogs) { 
+            return val.likes
+    }
+    }
+    else {
+     maxindex = 0   
+     sumOfLikes = 0
+     for (var [index, val] of blogs.entries()) { 
+        if (val.likes > sumOfLikes) {
+            maxindex = index
+            sumOfLikes = val.likes
+        }
+        
+}  
+return blogs[maxindex]
+    }
+    
+}  
 
   module.exports = {
     dummy,
-    totalLikes
-  }
+    totalLikes,
+    favoriteBlog
+}
