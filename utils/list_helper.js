0c6cb7fa0f45da030779c244f6279a169bdb3684
Greg <gkowal@protonmail.com>
2021-10-05 21:11:01 +0200

    4.4 module done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,32 @@ const dummy = (blogs) => {
     return 1 
   }
   
+
+const totalLikes =  (blogs) => {
+    if (blogs.length === 1) {
+    for (const val of blogs) { 
+            return val.likes
+    }
+    }
+    else {
+     sumOfLikes = 0
+     for (const val of blogs) { 
+        sumOfLikes = sumOfLikes + val.likes
+        
+}  
+    return sumOfLikes
+    }
+    
+       
+      
+
+      
+
+}  
+
+
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
