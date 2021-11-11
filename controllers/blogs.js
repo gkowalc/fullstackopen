7bef560c6f489e69e42af12135e39a2f736eba39
Greg <gkowal@protonmail.com>
2021-11-11 15:41:59 +0100

    4.9*

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,25 +2,26 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then(blogs => {
-      response.json(blogs.map(blogs => blogs.toJSON()))
-    })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}) 
+   response.json(blogs) 
+    
+    
   })
   
-  blogsRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-      .then(blog => {
+  blogsRouter.get('/:id', async (request, response, next) => {
+    const blog = await   Blog.findById(request.params.id)
+  
+
+      
         if (blog) {
-          response.json(blog.toJSON())
+          response.json(blog)
         } else {
           response.status(404).end()
         }
-      })
-      .catch(error => next(error))
   })
   
-  blogsRouter.post('/', (request, response, next) => {
+  blogsRouter.post('/',  async (request, response, next) => {
     const body = request.body
    
     const blog = new Blog({
@@ -28,16 +29,15 @@ blogsRouter.get('/', (request, response) => {
         author: body.author,
         url: body.url,
         likes: body.likes
+        
     })
 
-    blog.save()
-      .then(savedBlog => {
-        response.json(savedBlog.toJSON())
-      })
-      .catch(error => next(error))
+   const savedblog = await  blog.save()
+        response.json(savedblog)
   })
+
   
-  blogsRouter.delete('/:id', (request, response, next) => {
+  blogsRouter.delete('/:id',  async (request, response, next) => {
     Blog.findByIdAndRemove(request.params.id)
       .then(() => {
         response.status(204).end()
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+
+
 
 test('notes are returned as json', async () => {
   await api
@@ -11,16 +12,26 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 
 })
+test('number of blogs matches records in the database', async () => 
+{
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(3)
+
+
+})
+
+test('4.9e', async () => 
+{
+  const response = await api.get('/api/blogs')
+  console.log(response.body.toJSON)
+  expect(response.body[0].id).toBeDefined()
+
+
+})
+
 
 afterAll(() => {
   mongoose.connection.close()
 }, 100000)
 
 
-test('number of blogs matches records in the database', async () => 
-{
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(6)
-
-
-})
