70b4b1b803fff467980a805407cc0a037f6dd16d
Greg <gkowal@protonmail.com>
2021-11-15 15:46:16 +0100

    4.10 test added

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 
+const Blog = require('../models/blog')
 
 
 test('notes are returned as json', async () => {
@@ -26,6 +27,24 @@ test('4.9e', async () =>
   console.log(response.body.toJSON)
   expect(response.body[0].id).toBeDefined()
 
+
+})
+test('4.10', async () => 
+{
+  const NewBLog = {
+    title: 'ttest 1234',
+    author: 'author',
+    url: 'url',
+    likes: 4
+  }
+  const initialresponse = await api.get('/api/blogs')
+  const initialcontents = initialresponse.body.map(r => r.content)
+
+  await api.post('/api/blogs').send(NewBLog)
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map(r => r.title)
+  expect(contents).toHaveLength(initialcontents.length + 1) 
+  expect(contents).toContain('ttest 1234')
 
 })
 
