330372c5ef85f003b434acba46cfe141e5fd017b
Greg <gkowal@protonmail.com>
2021-11-16 14:29:05 +0100

    4.15 api/users api

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,8 +4,9 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-
-
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const helper = require('./test_helper')
 test('notes are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -60,7 +61,6 @@ test('4.11', async () =>
 
   await api.post('/api/blogs').send(NewBLog)
 
-
   const response = await api.get('/api/blogs')
   const contents = response.body.map(r => r.likes)
   expect(contents).toHaveLength(initialcontents.length + 1)
@@ -81,8 +81,39 @@ test('4.12', async () =>
 
 
 })
+// User tsts
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
 
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
 
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
 afterAll(() => {
   mongoose.connection.close()
 }, 100000)
