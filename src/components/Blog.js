716c616d69ca6c4df0b3a48d381e901e5ff95459
Greg <gkowal@protonmail.com>
2021-12-09 18:38:12 +0100

    5.8

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,5 @@
 import {React, useState} from 'react'
+import axios from 'axios'
 const Blog = ({blog}) => {
   
   const blogStyle = {
@@ -16,6 +17,47 @@ const Blog = ({blog}) => {
   const toggleVisibility = () => {
     setVisible(!visible)
   }
+
+  const create = async newObject => {
+    const retrievedtoken = JSON.parse(localStorage.getItem('loggedNoteappUser'))
+    const config = {
+      headers: { Authorization: `bearer ${retrievedtoken.token}`},
+    }
+    console.log(config)
+    const baseUrl = '/api/blogs/' + blog.id 
+    console.log(baseUrl)
+    const response = await axios.put(baseUrl, newObject, config)
+    
+    return response.data }
+
+  const likeButton = async () => {
+    
+    
+   
+     try {
+      
+      const likes = blog.likes + 1 
+      console.log(likes)
+      const newObject ={}
+     console.log(newObject)
+     const title = blog.title
+     const author = blog.author
+     const url = blog.url
+     const user = blog.user
+     console.log( title, author, url, user, likes)
+      const newblog = await create({title, author, url, user, likes, likes}
+        
+      ) 
+    
+     
+    } catch (exception) {
+      console.log('Something wrong' + exception)  
+
+
+
+
+    }
+  } 
   if (visible) {
     return(
       <div style={blogStyle}>
@@ -23,6 +65,8 @@ const Blog = ({blog}) => {
         
         {blog.author} <br></br>
         {blog.url} <br></br>
+        {blog.likes} 
+        <button onClick={likeButton}>like </button> <br></br>
         <button onClick={toggleVisibility}>cancel</button>
         </div>
     )
@@ -37,7 +81,7 @@ const Blog = ({blog}) => {
     ) 
   } 
 
-}
+} 
 export default Blog
 
 
