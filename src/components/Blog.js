42559e5b9caf036909717011000919aa849d1d87
Greg <gkowal@protonmail.com>
2021-12-09 22:00:32 +0100

    5.10* refactored

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -30,6 +30,30 @@ const Blog = ({blog}) => {
     
     return response.data }
 
+    const deleted = async newObject => {
+      const retrievedtoken = JSON.parse(localStorage.getItem('loggedNoteappUser'))
+      const config = {
+        headers: { Authorization: `bearer ${retrievedtoken.token}`},
+      }
+      console.log(config)
+      const baseUrl = '/api/blogs/' + blog.id 
+      console.log(baseUrl)
+      const response = await axios.delete(baseUrl, config)
+      
+      return response.data }
+
+const deleteEntry = async () => {
+  var confirm = window.confirm("Are you sure?");
+  if (confirm ==true) {
+    try {
+      return await deleted()
+     }
+     catch (exception) {
+      console.log('Something wrong' + exception)  
+  }
+  }
+  
+}
   const likeButton = async () => {
     
     
@@ -37,19 +61,16 @@ const Blog = ({blog}) => {
      try {
       
       const likes = blog.likes + 1 
-      console.log(likes)
-      const newObject ={}
-     console.log(newObject)
      const title = blog.title
      const author = blog.author
      const url = blog.url
      const user = blog.user
-     console.log( title, author, url, user, likes)
+ 
       const newblog = await create({title, author, url, user, likes, likes}
         
       ) 
     
-     
+
     } catch (exception) {
       console.log('Something wrong' + exception)  
 
@@ -62,12 +83,12 @@ const Blog = ({blog}) => {
     return(
       <div style={blogStyle}>
         {blog.title} <br></br>
-        
         {blog.author} <br></br>
         {blog.url} <br></br>
         {blog.likes} 
         <button onClick={likeButton}>like </button> <br></br>
         <button onClick={toggleVisibility}>cancel</button>
+        <button onClick={deleteEntry}>DELETE!</button>
         </div>
     )
   } 
